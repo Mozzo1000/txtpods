2dfd5ae41e0130182fc2aa82c6f32f1bcd49207c
Andreas Backström <andreas@simplymozzo.se>
2024-01-14 18:18:36 +0100

    Add API url as env variable

--- web/src/services/podcasts.service.jsx
+++ web/src/services/podcasts.service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://api.txtpods.org/v1/";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const get = (id) => {
     return axios.get(API_URL + "podcasts/" + id);
